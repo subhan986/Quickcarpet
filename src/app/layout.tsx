4349753ca0ac641c728275c272398ce3e650d187
Muhammad Subhan <subyounas@gmail.com>
2026-03-12 09:51:05 +0000

    use favicon.ico as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import SplashScreen from "@/components/splash-screen";
 export const metadata: Metadata = {
   title: "Quick Step Carpets & Upholstery Dry Cleaning",
   description: "Professional Cleaning For Carpets, Upholstery And Hard Floors.",
-  icons: [{ rel: "icon", url: "/LOGO.png" }],
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
